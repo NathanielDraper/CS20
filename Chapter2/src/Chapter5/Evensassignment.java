afcecde175a35a15f170266aafb9c41d4857b3ff
1005085003 <1005085003@L815WD220019.edu.cbe.ab.ca>
2022-03-28 09:58:27 -0600

    Evens Assignment Chapter 5

---
+++ Chapter2/src/Chapter5/Evensassignment.java
@@ -0,0 +1,36 @@
+/*
+
+Program: Assignment1Chapter5Evens        Last Date of this Revision: March 27th 2022
+
+Purpose: Create an application that displays even numbers between 1-20
+
+Author: Nathaniel Draper
+School: CHHS
+Course: Computer Science 20
+
+*/
+
+
+package Chapter5;
+
+public class Evensassignment {
+
+	public static void main(String[] args) {
+		System.out.println(""
+				
++"The even numbers beetwen 1 and 20 are: \n"
++"2\n"
++"4\n"
++ "6\n"
++ "8\n" 
++ "10\n" 
++ "12\n"  
++"14 \n"  
++"16 \n"
++ "18 \n"
++ "20  \n"
+); 		
+
+	}
+
+}
